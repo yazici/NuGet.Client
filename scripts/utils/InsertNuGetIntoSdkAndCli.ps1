974556064eca5e67e1c857601c9f394ae2531a6a
Donnie Goodson <donnie.goodson@microsoft.com>
2019-06-12 15:06:00 -0700

    Error in CI due to XML Content string parsing issue

--- scripts/utils/InsertNuGetIntoSdkAndCli.ps1
+++ scripts/utils/InsertNuGetIntoSdkAndCli.ps1
@@ -103,8 +103,11 @@ Write-Host "******<<<<<<"
 Write-Host "*****repoOwner*****:$repoOwner"
 Write-Host "*****RepositoryName*****:$RepositoryName"
 Write-Host "*****TEST*****"
-$headerOutput = ($Headers.Keys | foreach { "$_ $($Headers[$_])" }) -join "|"
-Write-Host "*****Headers*****: $headerOutput"
+$headerOutput = $Headers | Out-String
+
+Write-Host "*****Headers*****:"
+Write-Host $headerOutput -ForegroundColor red
+
 Write-Host "*****Body*****:$Body"
 $r1 = Invoke-RestMethod -Headers $Headers -Method Post -Uri "https://api.github.com/repos/$repoOwner/$RepositoryName/git/refs" -Body $Body
 Write-Host $r1
