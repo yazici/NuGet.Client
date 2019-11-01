47927955e6d0f47d81cf117213e1eff9e0a54469
Heng Liu <liu.heng@microsoft.com>
2019-11-01 14:02:02 -0700

    replace UseDotNet@2 task with scripts

---
+++ scripts/utils/InstallCLIforBuild.ps1
@@ -0,0 +1,27 @@
+# Download the CLI install script to Agent.TempDirectory
+
+echo "Installing dotnet CLI into $(Agent.TempDirectory) folder for building"
+
+$InstallDir = Join-Path $(Agent.TempDirectory) 'dotnet'
+
+$DotNetInstall = Join-Path $InstallDir 'dotnet-install.ps1'
+
+Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile $DotNetInstall
+
+
+
+if ([Environment]::Is64BitOperatingSystem) 
+{
+    $arch = "x64";
+}
+else 
+{
+    $arch = "x86";
+}
+
+& $DotNetInstall -Channel master -i $InstallDir -Architecture $arch 
+
+
+# Display build info
+& dotnet --info
+
---
+++ scripts/utils/InstallCLIforBuild.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Download the CLI install script to Agent.TempDirectory
+
+echo "Installing dotnet CLI into $(Agent.TempDirectory) folder for building"
+
+installDir="$(Agent.TempDirectory)/dotnet"
+
+mkdir -p $installDir
+
+curl -o $installDir/dotnet-install.sh -L https://dot.net/v1/dotnet-install.sh
+
+# Run install.sh for cli
+
+chmod +x $installDir/dotnet-install.sh
+
+
+# install master channel to get latest .NET 5 sdks 
+
+$installDir/dotnet-install.sh -i $installDir -c master
+
+echo "Deleting .NET Core temporary files"
+rm -rf "/tmp/"dotnet.*
+
+
+# Display current version
+
+dotnet --info
+
+
+
+echo "================="
+
