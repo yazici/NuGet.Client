169547aabd55a2d30d2844bf78c15590470422d1
Heng Liu <liu.heng@microsoft.com>
2019-11-04 00:07:42 -0800

    fix

--- scripts/utils/InstallCLIforBuild.ps1
+++ scripts/utils/InstallCLIforBuild.ps1
@@ -1,8 +1,8 @@
 # Download the CLI install script to Agent.TempDirectory
 
-#Write-Host "Installing dotnet CLI into $(AGENT_TEMPDIRECTORY) folder for building"
+#Write-Host "Installing dotnet CLI into $Env:AGENT_TEMPDIRECTORY folder for building"
 
-$InstallDir = Join-Path $(AGENT_TEMPDIRECTORY) 'dotnet'
+$InstallDir = Join-Path $Env:AGENT_TEMPDIRECTORY 'dotnet'
 
 $DotNetInstall = Join-Path $InstallDir 'dotnet-install.ps1'
 
