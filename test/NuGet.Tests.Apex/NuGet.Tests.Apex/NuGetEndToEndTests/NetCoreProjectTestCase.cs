1dfc4c40e877d60121c22bdd9eba8e2c703156ed
Heng Liu <liu.heng@microsoft.com>
2019-12-04 13:28:43 -0800

    fix Apex test CreateNetCoreProject_AddProjectReference to clear msg

--- test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
+++ test/NuGet.Tests.Apex/NuGet.Tests.Apex/NuGetEndToEndTests/NetCoreProjectTestCase.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 using Microsoft.Test.Apex.VisualStudio.Solution;
 using NuGet.StaFact;
 using Xunit;
@@ -46,10 +47,45 @@ namespace NuGet.Tests.Apex
                 testContext.SolutionService.Build();
                 testContext.NuGetApexTestService.WaitForAutoRestore();
 
+                VisualStudio.AssertNoErrors();
                 CommonUtility.AssertPackageInAssetsFile(VisualStudio, testContext.Project, "TestProject2", "1.0.0", XunitLogger);
             }
         }
 
+        // make sure ClearWindows in ApexTestContext constructor will only clear OutputWindow at the beginning
+        [NuGetWpfTheory]
+        [MemberData(nameof(GetNetCoreTemplates))]
+        public void CreateNetCoreProject_InstallNonExistingPackageFail(ProjectTemplate projectTemplate)
+        {
+            // Arrange
+            EnsureVisualStudioHost();
+
+            using (var testContext = new ApexTestContext(VisualStudio, projectTemplate, XunitLogger))
+            {
+                VisualStudio.AssertNoErrors();
+
+                var project3 = testContext.SolutionService.AddProject(ProjectLanguage.CSharp, projectTemplate, ProjectTargetFramework.V46, "TestProject3");
+                project3.Build();
+
+                var nugetConsole = GetConsole(project3);
+
+                var packageName = "nonexistPackage";
+                var packageVersion = "1.0.1";
+
+                nugetConsole.InstallPackageFromPMC(packageName, packageVersion);
+
+                var outputList = VisualStudio.GetOutputWindowsLines();
+                StringBuilder sb = new StringBuilder();
+                foreach (var line in outputList)
+                {
+                    sb.AppendLine(line);
+                }
+  
+                Assert.True(outputList.Count > 0, "install a nonexist package failed, the outputlist of PMC should not be empty.");
+                Assert.True(sb.ToString().Contains("Unable to find package"), sb.ToString());
+
+            }
+        }
         // There  is a bug with VS or Apex where NetCoreConsoleApp and NetCoreClassLib create netcore 2.1 projects that are not supported by the sdk
         // Commenting out any NetCoreConsoleApp or NetCoreClassLib template and swapping it for NetStandardClassLib as both are package ref.
 
