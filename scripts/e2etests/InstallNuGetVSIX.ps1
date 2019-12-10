32c2fae1567a02114c8fba51a8dc8ea362ce3e75
Damon Tivel <dtivel@microsoft.com>
2019-12-10 11:42:45 -0800

    Improve logging.

--- scripts/e2etests/InstallNuGetVSIX.ps1
+++ scripts/e2etests/InstallNuGetVSIX.ps1
@@ -23,33 +23,60 @@ if ($success -eq $false) {
     exit 1
 }
 
+Function Write-HostWithTimestamp([string] $message)
+{
+    Write-Host "[$([System.DateTime]::UtcNow.ToString("O"))]  $message"
+}
+
+Function Write-RunningProcesses()
+{
+    $output = tasklist | Out-String
+
+    Write-HostWithTimestamp $output
+}
+
 $VSIXSrcPath = Join-Path $NuGetDropPath 'NuGet.Tools.vsix'
 $VSIXPath = Join-Path $FuncTestRoot 'NuGet.Tools.vsix'
 
+Write-HostWithTimestamp "Copying $VSIXSrcPath to $VSIXPath."
+
 Copy-Item $VSIXSrcPath $VSIXPath
+
+Write-HostWithTimestamp 'Copy complete.'
 
 # Because we are upgrading an installed system component VSIX, we need to downgrade first.
 $numberOfTries = 0
 $success = $false
 do {
+    Write-HostWithTimestamp 'Killing running instances of Visual Studio.'
+    Write-RunningProcesses
     KillRunningInstancesOfVS
+    Write-RunningProcesses
+    Write-HostWithTimestamp 'Killed running instances of Visual Studio.'
     $numberOfTries++
-    Write-Host "Attempt # $numberOfTries to downgrade VSIX..."
+    Write-HostWithTimestamp "Attempt (# $numberOfTries) to downgrade VSIX..."
     $success = DowngradeVSIX $NuGetVSIXID $VSVersion $ProcessExitTimeoutInSeconds
+    Write-HostWithTimestamp 'Attempt complete.'
 }
 until (($success -eq $true) -or ($numberOfTries -gt 3))
 
 # Clearing MEF cache helps load the right dlls for VSIX
+Write-HostWithTimestamp "Clearing MEF cache."
 ClearMEFCache
-
+Write-HostWithTimestamp "Cleared MEF cache."
 
 $numberOfTries = 0
 $success = $false
 do {
+    Write-HostWithTimestamp 'Killing running instances of Visual Studio.'
+    Write-RunningProcesses
     KillRunningInstancesOfVS
+    Write-RunningProcesses
+    Write-HostWithTimestamp 'Killed running instances of Visual Studio.'
     $numberOfTries++
-    Write-Host "Attempt # $numberOfTries to install VSIX..."
+    Write-HostWithTimestamp "Attempt (# $numberOfTries) to install VSIX..."
     $success = InstallVSIX $VSIXPath $VSVersion $ProcessExitTimeoutInSeconds
+    Write-HostWithTimestamp 'Attempt complete.'
 }
 until (($success -eq $true) -or ($numberOfTries -gt 3))
 
@@ -57,5 +84,9 @@ if ($success -eq $false) {
     exit 1
 }
 
+Write-HostWithTimestamp 'Clearing MEF cache.'
 ClearMEFCache
+Write-HostWithTimestamp 'Cleared MEF cache.'
+Write-HostWithTimestamp 'Updating Visual Studio configuration.'
 Update-Configuration
+Write-HostWithTimestamp 'Updated Visual Studio configuration.'
