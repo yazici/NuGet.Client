5478acb332e36641661fffdbe77237cc7865b434
Heng Liu <liu.heng@microsoft.com>
2020-01-10 13:16:41 -0800

    fix using and add header

--- test/TestUtilities/Test.Utility/Signing/CertificateStoreUtilities.cs
+++ test/TestUtilities/Test.Utility/Signing/CertificateStoreUtilities.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using NuGet.Common;
 
 namespace Test.Utility.Signing
